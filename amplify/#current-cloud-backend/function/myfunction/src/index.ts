5fd75fa642b58a0fe6cd17c9c568f8c3da54989e
popovdmy <dmitro.popov@looming.tech>
2023-03-07 13:00:28 +0200

    Push last changes in index.ts with handler;

--- amplify/#current-cloud-backend/function/myfunction/src/index.ts
+++ amplify/#current-cloud-backend/function/myfunction/src/index.ts
@@ -4,7 +4,7 @@ import { app } from './app'
 
 const server = awsServerlessExpress.createServer(app);
 
-export const lambdaHandler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
+export const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
     console.log(`Event: ${JSON.stringify(event, null, 2)}`);
     console.log(`Context: ${JSON.stringify(context, null, 2)}`);
     return awsServerlessExpress.proxy(server, event, context, 'PROMISE').promise;
